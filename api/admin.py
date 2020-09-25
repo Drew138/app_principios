eaa2917de4197a7ef0d42cb2958c19d3dcb868c2
drew <andresalazar138@gmail.com>
2020-09-25 11:32:26 -0500

    added database models

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+admin.site.register(models.Ciudad)
+admin.site.register(models.Producto)
+admin.site.register(models.Orden)
+admin.site.register(models.Usuario)
--- api/models.py
+++ api/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Ciudad(models.Model):
+
+    nombre = models.CharField(max_length=15)
+    departamento = models.CharField(max_length=15)
+
+
+class Usuario(AbstractUser):
+
+    MASCULINO = 'M'
+    FEMENINO = 'F'
+    INDEFINIDO = 'I'
+    OPCIONES_GENERO = ((MASCULINO, 'Masculino'),
+                       (FEMENINO, 'Femenino'),
+                       (INDEFINIDO, 'Indefinido'),
+                       )
+    COMPRADOR = 'C'
+    VENDEDOR = 'V'
+    OPCIONES_TIPO = ((COMPRADOR, 'Comprador'),
+                     (VENDEDOR, 'Vendedor'),
+                     )
+
+    genero = models.CharField(
+        max_length=1, choices=OPCIONES_GENERO, default=INDEFINIDO)
+    telefono = models.IntegerField(default=0)
+    direccion = models.CharField(max_length=30)
+    ciudad = models.ForeignKey(
+        Ciudad, related_name="compradores", on_delete=models.SET_NULL, blank=True, null=True)
+    establecimiento = models.CharField(max_length=30, default="n/a")
+    tipo = models.CharField(max_length=1, default=COMPRADOR)
+
+
+class Producto(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    precio = models.IntegerField()
+    marca = models.CharField(max_length=15)
+    disponibilidad = models.IntegerField(default=0)
+    vendedor = models.ForeignKey(
+        Usuario, related_name="productos", on_delete=models.CASCADE, null=True, blank=True)
+
+
+class Orden(models.Model):
+
+    comprador = models.ForeignKey(
+        Usuario, related_name="orden_vend", on_delete=models.SET_NULL, blank=True, null=True)
+    costo = models.IntegerField()
+    cantidad = models.IntegerField(default=0)
+    producto = models.ForeignKey(
+        Producto, related_name="orden_prod", on_delete=models.SET_NULL, blank=True, null=True)
