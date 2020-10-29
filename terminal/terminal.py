6ec0c0b87b48ee70345a5b66b4308663a64b9880
Alejandro Torres <alejandrotorres@MacBook-Air-de-Alejandro.local>
2020-10-28 22:45:56 -0500

    AVANCE

--- terminal/terminal.py
+++ terminal/terminal.py
@@ -1,5 +1,7 @@
 import requests
 import os
+import prettytable
+import getpass
 
 class Terminal:
     def __init__(self):
@@ -9,11 +11,24 @@ class Terminal:
         self.vendedor = ""
         self.host = "http://127.0.0.1.8000"
         
+    def setContraseña(self, contaseña):
+        self.contraseña = contraseña
+
+    def setUsuario(self, usuario):
+        self.usuario = usuario
+
+    def setJWT(self, jwt):
+        self.jwt = jwt
 
     def autenticar(self):
-       url = " "
-       direccion = os.path.join(self.host,url) + "/"
-       response = requests.post(self.host+)
+        usuario = input("Ingrese su usuario: ")
+        contraseña = getpass.getpass("Ingrese su contraseña: ")
+        self.setUsuario(usuario)
+        self.setContraseña(contraseña)
+        credenciales = {"username": self.usuario, "password": self.contraseña}
+        url = " "
+        direccion = os.path.join(self.host,url) + "/"
+        response = requests.post(direccion, credenciales) 
 
     def enviarInformacion(self):
         pass
@@ -24,14 +39,31 @@ class Terminal:
     def verCompradores(self):
         pass
 
-    def mostrarInformacionUsuario(self):
-        pass
+    def mostrarInformacionComprador(self):
+        table = prettytable()
+        table.fieldnames= ["Marca", "Producto", "Precio", "Disponibilidad"]
+        # TODO Mostrar información comprador 
+        print(table)
+
+    def mostrarInformacionVendedor(self):
+        table = prettytable()
+        table.fieldnames= ["Marca", "Producto", "Precio", "Disponibilidad"]
+        # TODO Mostrar información vendedor 
+        print(table)
+
 
     def mostrarDisponibilidadProducto(self):
-        pass
+        table = prettytable()
+        table.fieldnames= ["Marca", "Producto", "Precio", "Disponibilidad"]
+        # TODO Hacer solicitud de productos
+        print(table)
+        
 
     def mostrarInformacionOrden(self):
-        pass
+        table = prettytable()
+        table.fieldnames= ["Productos", "Cantidades", "Costos"]
+        # TODO Mostrar información de órden
+        print(table)
 
     def verificarInformacionUsurio(self):
         pass
